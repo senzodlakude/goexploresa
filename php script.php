77b75dae8aea89bb439c3fa40a6d938870115d87
senzotech <144389493+senzodlakude@users.noreply.github.com>
2025-12-14 22:38:31 +0200

    Add files via upload

---
+++ php script.php
@@ -0,0 +1,15 @@
+<?php
+// sitemap-generator.php
+header('Content-Type: application/xml; charset=utf-8');
+echo '<?xml version="1.0" encoding="UTF-8"?>';
+?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    <?php foreach($businesses as $business): ?>
+    <url>
+        <loc>https://goexploresa.co.za/business/<?= $business['slug'] ?></loc>
+        <lastmod><?= $business['updated_at'] ?></lastmod>
+        <changefreq>weekly</changefreq>
+        <priority>0.6</priority>
+    </url>
+    <?php endforeach; ?>
+</urlset>
